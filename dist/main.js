f269814fc34411bec5d85f7d26b2041edd839d90
Hasitha Madhusanka <HasithaM@empite.corp>
2017-10-04 09:37:48 +0530

    added map

--- dist/main.js
+++ dist/main.js
@@ -1,58 +1,12 @@
-'use strict';
+"use strict";
 
 System.register([], function (_export, _context) {
 	"use strict";
 
-	var colors, index, element, numbers, sum, sum, numbers, DoubleNumbers, output;
-
-
-	function hasitha() {
-		console.log("hasitha");
-	}
-
-	//using a function inside forEach
-	function adder(element) {
-		sum += element;
-	}
+	var numbers, DoubleNumbers;
 	return {
 		setters: [],
 		execute: function () {
-			console.log("video_04========================================================");
-			colors = ['red', 'blue'];
-
-
-			//=====================================================================
-			//normal way
-			for (index = 0; index < colors.length; index++) {
-				element = colors[index];
-
-				console.log(element);
-			}
-
-			//=====================================================================
-			//array helpers
-			//KEY WORDS ==>> iterater function
-			//PROS ==>> less code
-			colors.forEach(function (color) {
-				console.log(color);
-			});
-
-			//array helpers and after for each calling another function
-			colors.forEach(function (color) {
-				console.log(color);
-			}, hasitha());console.log("video_05========================================================");
-
-			numbers = [1, 2, 3, 4];
-			sum = 0;
-
-			numbers.forEach(function (element) {
-				sum += element;
-			});
-			console.log(sum);sum = 0;
-
-			numbers.forEach(adder);
-			console.log(sum);
-
 			console.log("video_07==(MAP)==================================================");
 
 			numbers = [1, 2, 3];
@@ -64,10 +18,6 @@ System.register([], function (_export, _context) {
 			console.log(DoubleNumbers);
 
 			console.log("video_08==(MAP more COMPLEX)=====================================");
-
-			output = document.getElementById('output');
-
-			output.innerHTML += 'Application';
 		}
 	};
 });
