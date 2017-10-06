becd2f60e349674460abc1d8611dc3b6c4318d32
Hasitha Madhusanka <HasithaM@empite.corp>
2017-10-06 14:16:34 +0530

    why map used to

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-console.log("video_07==(MAP)==================================================");
+//map used to render list of data
+console.log("07==(MAP)==================================================");
 
 var numbers = [1,2,3];
 var DoubleNumbers = numbers.map(function (number) {
@@ -8,7 +9,7 @@ var DoubleNumbers = numbers.map(function (number) {
 console.log(numbers);
 console.log(DoubleNumbers);
 
-console.log("video_08==(MAP more COMPLEX)=====================================");
+console.log("08==(MAP more COMPLEX)=====================================");
 
 var cars = [
 	{model:'Toyota',price:100000},
